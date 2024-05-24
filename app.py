5bee49f276de2a62d7699d3fd11b8bbd086f49e5
Manuel <manuel14.provenzi@gmail.com>
2024-05-24 12:51:53 +0200

    test immagini dinamiche

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 from food_api import FoodAPI
+from image import Image
+import os
+
 
 app = Flask(__name__)
 
@@ -11,6 +14,21 @@ def index():
 @app.route('/game')
 def game():
     food = FoodAPI()
-    img_url,category = food.get_random_food()
-    food.saveImage(img_url)
-    return render_template('game.html')
+    img_url, category = food.get_random_food()
+    path_img = food.saveImage(img_url)
+    actual_image = Image(path_img, category)
+    
+    images_all = []
+    images_dir = os.path.join(app.static_folder, 'food_images')
+    
+    subdirectories = [name for name in os.listdir(images_dir) if os.path.isdir(os.path.join(images_dir, name))]
+    
+    for subdir in subdirectories:
+        for filename in os.listdir(f"{images_dir}/{subdir}"):
+            if filename.endswith('.jpg') or filename.endswith('.png'):
+                image_path = os.path.join(f"{images_dir}/{subdir}", filename)
+                i = Image(image_path, subdir)
+                images_all.append(i)
+    
+    return render_template('game.html', images=images_all, photo=actual_image)
+
---
+++ image.py
@@ -0,0 +1,5 @@
+class Image:
+    def __init__(self, path,categoria):
+        self.path = path
+        self.categoria = categoria
+        pass
