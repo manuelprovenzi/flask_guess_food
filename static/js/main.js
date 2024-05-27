9856576cc902745032c428b512274d12250d4eed
Manuel <manuel14.provenzi@gmail.com>
2024-05-27 19:35:52 +0200

    javascript aggiunto per pagine sprovviste

--- static/js/main.js
+++ static/js/main.js
@@ -9,3 +9,11 @@ function getClassifica(){
 function logout(){
     location.href = "/logout";
 }
+
+window.addEventListener('beforeunload', function (e) {
+    // Invio una richiesta al server quando la scheda sta per essere chiusa
+    navigator.sendBeacon('/window_closed', JSON.stringify({ message: 'La scheda è stata chiusa.' }));
+
+    // Se vuoi mostrare un messaggio di conferma di uscita
+    // e.returnValue = '';
+});
